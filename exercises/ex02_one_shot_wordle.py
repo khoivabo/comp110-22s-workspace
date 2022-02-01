8e29996b07eea6496c42661c5cd521327e14dc05
Khoi Bui <kqbui@ad.unc.edu>
2022-02-01 13:10:26 -0500

    Finally done pls

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -11,7 +11,7 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001f7E8"
 
 while len(player_guess) != len(secret_word):
-    player_guess: str = (input(f"That was not {len(secret_word)} letters! Try again: "))
+    player_guess = (input(f"That was not {len(secret_word)} letters! Try again: "))
 index_guess: str = (player_guess[0])
 correct_letter: str = GREEN_BOX 
 player_counter: int = 0
@@ -23,8 +23,6 @@ while player_counter < len(secret_word):
     if player_guess[player_counter] == secret_word[player_counter]:
         color_boxes = color_boxes + GREEN_BOX
     elif player_guess[player_counter] != secret_word[player_counter]:
-        secret_bool: bool = False 
-        secret_check: int = 0 
         while secret_bool == (False) and secret_check < len(secret_word):
             if player_guess[player_counter] == secret_word[secret_check]:
                 secret_check = secret_check + len(secret_word) 
