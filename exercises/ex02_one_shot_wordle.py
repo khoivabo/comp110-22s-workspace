e6b51d3d727e4ce8b170b26dbb21e90b617c2ba3
Khoi Bui <kqbui@ad.unc.edu>
2022-02-01 14:38:05 -0500

    me 100

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -3,40 +3,46 @@
 __author__ = "730277137"
 
 
+from readline import write_history_file
+
+
 secret_word: str = ("python")
 player_guess: str = (input(f"What is your {len(secret_word)}-letter guess? "))
+played: bool = False
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001f7E8"
 
-while len(player_guess) != len(secret_word):
-    player_guess = (input(f"That was not {len(secret_word)} letters! Try again: "))
-index_guess: str = (player_guess[0])
-correct_letter: str = GREEN_BOX 
-player_counter: int = 0
-secret_counter: int = 0
-color_boxes: str = ("")
-secret_bool: bool = False 
-secret_check: int = 0 
-while player_counter < len(secret_word):
-    if player_guess[player_counter] == secret_word[player_counter]:
-        color_boxes = color_boxes + GREEN_BOX
-    elif player_guess[player_counter] != secret_word[player_counter]:
-        while secret_bool == (False) and secret_check < len(secret_word):
-            if player_guess[player_counter] == secret_word[secret_check]:
-                secret_check = secret_check + len(secret_word) 
-                secret_bool = True
-            elif player_guess[player_counter] != secret_word[secret_check]:
-                secret_check = secret_check + 1
-        if secret_bool:
-            color_boxes = color_boxes + YELLOW_BOX
+word_index: int = 0
+emoji: str = ""
+
+
+while (not played):
+    while (len(player_guess) != len(secret_word)):
+        player_guess = (input(f"That was not {len(secret_word)} letters! Try again: "))
+    while(word_index < len(secret_word)):
+        foobar: bool = False
+        foobar_tracker: int = 0
+        if(player_guess[word_index] != secret_word[word_index]):
+            while not foobar and foobar_tracker < len(secret_word):
+                if player_guess[word_index] == secret_word[foobar_tracker]:
+                    foobar = True
+                    print(player_guess[word_index])
+                else:
+                    foobar_tracker += 1
+                    print(player_guess[word_index])
+            if foobar:
+                emoji += YELLOW_BOX
+            else:
+                emoji += WHITE_BOX
         else: 
-            color_boxes = color_boxes + WHITE_BOX
-    player_counter = player_counter + 1
-print(color_boxes)
-if player_guess == secret_word:
-    print("Woo! You got it!")
-else:
-    print("Not quite. Play again soon!")
-    exit()
+            emoji += GREEN_BOX
+        word_index += 1
+    print(emoji)
+    if (player_guess != secret_word):
+        print("Not quite. Play again soon!")
+        played = True
+    else: 
+        print("Woo! You got it!")
+        played = True
